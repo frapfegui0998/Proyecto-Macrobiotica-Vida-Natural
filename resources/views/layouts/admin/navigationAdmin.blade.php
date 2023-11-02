7bdcaef3cf9098f1412f5d5c6070001c1cec6cd0
HenryM8 <henryrm8@gmail.com>
2023-11-01 23:01:33 -0700

    Correccion de navegacion

--- resources/views/layouts/admin/navigationAdmin.blade.php
+++ resources/views/layouts/admin/navigationAdmin.blade.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" href="{{('fontawesome-free-6.4.2-web/css/all.min.css')}}">
+{{-- <link rel="stylesheet" href="{{('fontawesome-free-6.4.2-web/css/all.min.css')}}"> --}}
+<link rel="stylesheet" href="{{asset('fontawesome-free-6.4.2-web/css/all.min.css')}}">
 <style>
     .menu-item {
         display: block;
