8953f1888a91ce9cde2e24ab859b818a47ebdba2
frapfegui0998 <123138141+frapfegui0998@users.noreply.github.com>
2023-11-28 14:10:45 -0600

    Update paginaPrincipal.blade.php
    cambios a pagina principal

--- resources/views/paginaPrincipal.blade.php
+++ resources/views/paginaPrincipal.blade.php
@@ -2,13 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Macrobiótica Vida Natural</title>
-    <link rel="stylesheet" href="{{ asset('fontawesome-free-6.4.2-web/css/all.min.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -16,85 +15,31 @@
             background-size: cover;
             background-position: center center;
             margin: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
+            padding: 0;
             min-height: 100vh;
         }
 
-        .owl-carousel {
-            width: 80%;
-            margin: auto;
-            /* Para centrar el carousel */
-            border: 15px solid #ffff;
-            /* Agrega un contorno blanco alrededor */
+        .carousel-container {
+            max-width: 70%; 
+            margin: 90px auto 20px;
         }
 
-        .carousel-img {
-            width: 100%;
-            height: 350px;
-            display: block;
-        }
-
-        .owl-prev,
-        .owl-next {
-            font-size: 24px;
-            color: #fff;
-            background: #000;
-            padding: 10px;
-            border-radius: 50%;
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-        }
     </style>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+</head>    
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
 
+<header class="font-sans antialiased">
+    @include('layouts.navigationPaginaPrincipal')
+</header>
 <body>
-    <nav class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            <div class="flex-col md:flex md:flex-row md:min-h-screen">
-                @include('navigationPaginaPrincipal')
-            </div>
-        </div>
-    </nav>
 
-    <div class="owl-carousel">
-        <div><img class="carousel-img" src="{{ asset('images/Producto1.jpg') }}" alt="Imagen 1"></div>
-        <div><img class="carousel-img" src="{{ asset('images/Producto2.jpg') }}" alt="Imagen 2"></div>
-        <div><img class="carousel-img" src="{{ asset('images/Producto3.jpg') }}" alt="Imagen 3"></div>
-        <div><img class="carousel-img" src="{{ asset('images/Producto4.jpg') }}" alt="Imagen 4"></div>
+    <div class="container mt-4">
+        <div class="carousel-container px-3 px-md-4">
+            @include('layouts.carousel')
+        </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('.owl-carousel').owlCarousel({
-                loop: true,
-                margin: 10,
-                nav: true, // Muestra las flechas de navegación
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    600: {
-                        items: 3
-                    },
-                    1000: {
-                        items: 4
-                    }
-                }
-            });
-        });
-    </script>
 </body>
 
 </html>
