df19d64be067401b243e2fc41ba75d39bdbef767
HenryM8 <henryrm8@gmail.com>
2023-12-09 20:43:57 -0600

    Ajustes finales

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::select('id', 'name', 'email')->get();
         return view('admin.users.index', compact('users'));
     }
 
--- resources/views/layouts/admin/navigationAdmin.blade.php
+++ resources/views/layouts/admin/navigationAdmin.blade.php
@@ -1,5 +1,5 @@
 {{-- <link rel="stylesheet" href="{{('fontawesome-free-6.4.2-web/css/all.min.css')}}"> --}}
-<link rel="stylesheet" href="{{asset('fontawesome-free-6.4.2-web/css/all.min.css')}}">
+<link rel="stylesheet" href="{{ asset('fontawesome-free-6.4.2-web/css/all.min.css') }}">
 <style>
     .menu-item {
         display: block;
@@ -19,9 +19,12 @@
     }
 </style>
 
-<div @click.away="open = false" class="flex flex-col flex-shrink-0 w-full text-black bg-gray-800 md:w-64 dark-mode:text-gray-200 dark-mode:bg-gray-800" x-data="{ open: false }">
+<div @click.away="open = false"
+    class="flex flex-col flex-shrink-0 w-full text-black bg-gray-800 md:w-64 dark-mode:text-gray-200 dark-mode:bg-gray-800"
+    x-data="{ open: false }">
     <div class="flex flex-row items-center justify-between flex-shrink-0 px-8 py-4" style="background-color: #4267B2;">
-        <a href="{{ route('admin.index') }}" class="text-lg font-semibold tracking-widest text-white uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">
+        <a href="{{ route('admin.index') }}"
+            class="text-lg font-semibold tracking-widest text-white uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">
             Macrobiótica Vida Natural
         </a>
         <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
@@ -29,30 +32,40 @@
             </svg>
         </button>
     </div>
-    <nav :class="{ 'block': open, 'hidden': !open }" class="flex-grow px-4 pb-4 md:block md:pb-0 md:overflow-y-auto" style="background-color: #f0f2f5;">
-        <a class="menu-item {{ request()->is('admin') ? 'bg-blue-200' : 'bg-transparent' }}" href="{{ route('admin.index') }}">
+    <nav :class="{ 'block': open, 'hidden': !open }" class="flex-grow px-4 pb-4 md:block md:pb-0 md:overflow-y-auto"
+        style="background-color: #f0f2f5;">
+        <a class="menu-item {{ request()->is('admin') ? 'bg-blue-200' : 'bg-transparent' }}"
+            href="{{ route('admin.index') }}">
             <i class="fas fa-home" style="margin-right: 8px;"></i> Inicio
         </a>
-        <a class="menu-item {{ request()->is('admin/users*') ? 'bg-blue-200' : 'bg-transparent' }}" href="{{ route('admin.users.index') }}">
+        <a class="menu-item {{ request()->is('admin/users*') ? 'bg-blue-200' : 'bg-transparent' }}"
+            href="{{ route('admin.users.index') }}">
             <i class="fas fa-users" style="margin-right: 8px;"></i> Usuarios
         </a>
-        <a class="menu-item {{ request()->is('admin/roles*') ? 'bg-blue-200' : 'bg-transparent' }}" href="{{ route('admin.roles.index') }}">
+        <a class="menu-item {{ request()->is('admin/roles*') ? 'bg-blue-200' : 'bg-transparent' }}"
+            href="{{ route('admin.roles.index') }}">
             <i class="fas fa-user-tag" style="margin-right: 8px;"></i> Roles
         </a>
-        <a class="menu-item {{ request()->is('admin/permissions*') ? 'bg-blue-200' : 'bg-transparent' }}" href="{{ route('admin.permissions.index') }}">
+        <a class="menu-item {{ request()->is('admin/permissions*') ? 'bg-blue-200' : 'bg-transparent' }}"
+            href="{{ route('admin.permissions.index') }}">
             <i class="fas fa-key" style="margin-right: 8px;"></i> Permisos
         </a>
-        <a class="menu-item {{ request()->is('admin/products*') ? 'bg-blue-200' : 'bg-transparent' }}" href="{{ route('admin.products.index') }}">
+        <a class="menu-item {{ request()->is('admin/products*') ? 'bg-blue-200' : 'bg-transparent' }}"
+            href="{{ route('admin.products.index') }}">
             <i class="fas fa-box-open" style="margin-right: 8px;"></i> Productos
         </a>
 
         <!-- ... Otros elementos de menú ... -->
 
         <div @click.away="open = false" class="relative" x-data="{ open: false }">
-            <button @click="open = !open" x-bind:class="{'menu-item': true, 'bg-blue-200': open, 'bg-transparent': !open}">
+            <button @click="open = !open"
+                x-bind:class="{ 'menu-item': true, 'bg-blue-200': open, 'bg-transparent': !open }">
                 <i class="fas fa-user-cog" style="margin-right: 8px;"></i> <span> {{ Auth::user()->name }}</span>
-                <svg fill="currentColor" viewBox="0 0 20 20" :class="{ 'rotate-180': open, 'rotate-0': !open }" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
-                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                <svg fill="currentColor" viewBox="0 0 20 20" :class="{ 'rotate-180': open, 'rotate-0': !open }"
+                    class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                    <path fill-rule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clip-rule="evenodd"></path>
                 </svg>
             </button>
             <div x-show="open" x-transition:enter="transition ease-out duration-100"
@@ -74,8 +87,8 @@
                             {{ __('Log Out') }}
                         </x-dropdown-link>
                     </form>
+                </div>
             </div>
-        </div>
 
     </nav>
 </div>
