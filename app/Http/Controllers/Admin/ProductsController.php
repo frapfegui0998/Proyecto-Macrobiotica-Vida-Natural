50a295ad58e01e81e66d8d38d938f35cf9452f64
HenryM8 <henryrm8@gmail.com>
2024-04-14 21:56:08 -0600

    Try catch para todas las funciones del controller Products

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use App\Http\Requests\ProductRequest;
+use App\Models\LogError;
+use Exception;
 
 class ProductsController extends Controller
 {
@@ -39,9 +41,39 @@ class ProductsController extends Controller
         $validatedData = $request->validated();
 
         Products::create($validatedData);
-        /*return redirect()->route('admin.products.index')->with('success', 'Producto creado con éxito');*/
         return to_route('admin.products.index')->with('message', 'Producto creado con éxito.');
     }
+
+    public function story(ProductRequest $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'description' => ['string', 'max:255'],
+                'price' => ['required', 'numeric'],
+                'stock_quantity' => ['required', 'numeric'],
+                'image_url' => ['required', 'string', 'max:255'],
+            ]);
+
+            $validatedData = $request->validated();
+
+            Products::create($validatedData);
+            return redirect()->route('admin.products.index')->with('message', 'Producto creado con éxito.');
+        } catch (Exception $e) {
+            $errorMessage = $e->getMessage();
+
+            LogError::create([
+                'message' => 'Error al insertar un producto',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return back()->with('error', 'Ha ocurrido un error');
+        }
+    }
+
 
     public function edit(Products $product)
     {
@@ -50,25 +82,52 @@ class ProductsController extends Controller
     }
 
     public function update(ProductRequest $request, Products $product)
-{
-    $validated = $request->validate([
-        'name' => ['required', 'max:255', 'string'],
-        'description' => ['required', 'max:255', 'string'],
-        'price' => ['required', 'numeric', 'min:0'],
-        'stock_quantity' => ['required', 'integer', 'min:0'],
-        'image_url' => ['required', 'max:255', 'string'],
-    ]);
+    {
+        try {
+            $validated = $request->validate([
+                'name' => ['required', 'max:255', 'string'],
+                'description' => ['required', 'max:255', 'string'],
+                'price' => ['required', 'numeric', 'min:0'],
+                'stock_quantity' => ['required', 'integer', 'min:0'],
+                'image_url' => ['required', 'max:255', 'string'],
+            ]);
 
-    $product->update($validated);
+            $product->update($validated);
 
-    return redirect()->route('admin.products.index')->with('updated', 'Producto actualizado con éxito.');
-}
+            return redirect()->route('admin.products.index')->with('updated', 'Producto actualizado con éxito.');
+        } catch (Exception $e) {
+            $errorMessage = $e->getMessage();
 
+            LogError::create([
+                'message' => 'Error al actualizar un producto',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return back()->with('error', 'Ha ocurrido un error');
+        }
+    }
 
     public function destroy(Products $product)
     {
-        $product->delete();
+        try {
+            $product->delete();
 
-        return back()->with('deleted', 'Producto eliminado con éxito.');
+            return back()->with('deleted', 'Producto eliminado con éxito.');
+        } catch (Exception $e) {
+            $errorMessage = $e->getMessage();
+
+            LogError::create([
+                'message' => 'Error al eliminar un producto',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return back()->with('error', 'Ha ocurrido un error');
+        }
     }
 }
