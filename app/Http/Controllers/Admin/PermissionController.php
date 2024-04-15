0b98f1d656b90f74d85289e6fc9fb9c69c4c189e
HenryM8 <henryrm8@gmail.com>
2024-04-14 22:16:13 -0600

    Try Catch para todas las funciones del controller Permission

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -8,6 +8,7 @@ use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use App\Http\Requests\PermissionsRequest;
 use App\Models\LogError;
+use Exception;
 
 class PermissionController extends Controller
 {
@@ -50,7 +51,7 @@ class PermissionController extends Controller
             // ]);
 
             // Redirect back with error message
-            return redirect()->back()->withErrors(['error' => 'Ha ocurrido un error. Por favor, inténtalo de nuevo más tarde.']);
+            return back()->with('error', 'Ha ocurrido un error');
         }
     }
 
@@ -62,34 +63,99 @@ class PermissionController extends Controller
 
     public function update(PermissionsRequest $request, Permission $permission)
     {
-        $validated = $request->validate(['name' => ['required', 'min:3']]);
-        $permission->update($validated);
+        try {
+            $validated = $request->validate(['name' => ['required', 'min:3']]);
+            $permission->update($validated);
 
-        return to_route('admin.permissions.index')->with('updated', 'Permiso actualizado con éxito.');
+            return to_route('admin.permissions.index')->with('updated', 'Permiso actualizado con éxito.');
+        } catch (Exception $e) {
+            $errorMessage = $e->getMessage();
+
+            LogError::create([
+                'message' => 'Error al actualizar un permiso',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return back()->with('error', 'Ha ocurrido un error');
+        }
     }
+
 
     public function destroy(Permission $permission)
     {
-        $permission->delete();
+        try {
+            $permission->delete();
 
-        return back()->with('deleted', 'Permiso eliminado con éxito.');
+            return back()->with('deleted', 'Permiso eliminado con éxito.');
+        } catch (Exception $e) {
+            $errorMessage = $e->getMessage();
+
+            LogError::create([
+                'message' => 'Error al eliminar un permiso',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return back()->with('error', 'Ha ocurrido un error');
+        }
     }
 
     public function assignRole(Request $request, Permission $permission)
     {
-        if ($permission->hasRole($request->role)) {
-            return back()->with('message', 'El Rol ya existe.');
+        try {
+            if ($permission->hasRole($request->role)) {
+                return back()->with('message', 'El Rol ya existe.');
+            }
+            $permission->assignRole($request->role);
+            return back()->with('message', 'Rol Asignado.');
+        } catch (Exception $e) {
+
+            $errorMessage = $e->getMessage();
+
+            LogError::create([
+                'message' => 'Error al asignar un rol a un permiso',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return back()->with('error', 'Ha ocurrido un error');
         }
-        $permission->assignRole($request->role);
-        return back()->with('message', 'Rol Asignado.');
     }
 
     public function removeRole(Permission $permission, Role $role)
     {
-        if ($permission->hasRole($role)) {
-            $permission->removeRole($role);
-            return back()->with('message', 'Rol removido.');
+        try {
+            // Verificar si el usuario tiene el rol de administrador y si es así, no permitir su eliminación
+            if ($role->name === 'admin') {
+                return back()->with('deleted', 'El rol de administrador no puede ser removido.');
+            }
+
+            if ($permission->hasRole($role)) {
+                $permission->removeRole($role);
+                return back()->with('deleted', 'Rol removido.');
+            }
+
+            return back()->with('deleted', 'Este Rol no está asignado.');
+        } catch (Exception $e) {
+
+            $errorMessage = $e->getMessage();
+
+            LogError::create([
+                'message' => 'Error al remover un rol de un permiso',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return back()->with('error', 'Ha ocurrido un error');
         }
-        return back()->with('message', 'Este Rol no está asignado.');
     }
 }
