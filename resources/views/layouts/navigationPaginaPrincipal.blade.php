dcb4323a9959194eb8e40cb28e5e5462c25a8d0a
HenryM8 <henryrm8@gmail.com>
2024-04-17 22:45:41 -0600

    Restauración de ChatBot

--- resources/views/layouts/navigationPaginaPrincipal.blade.php
+++ resources/views/layouts/navigationPaginaPrincipal.blade.php
@@ -71,6 +71,20 @@
         }
     </style>
 </head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/656cf3c3ff45ca7d4786774e/1hgopgcob';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
 
 <body>
 
