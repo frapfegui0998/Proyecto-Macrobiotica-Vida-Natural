e607b74a0a15884536bae996217020276a245cf0
HenryM8 <henryrm8@gmail.com>
2024-04-25 22:11:25 -0600

    New Function

--- app/Http/Controllers/Correo/CorreoController.php
+++ app/Http/Controllers/Correo/CorreoController.php
@@ -49,4 +49,42 @@ class CorreoController extends Controller
         }
         return redirect()->back()->with('deleted', 'Ha ocurrido un error al enviar el correo electrónico. Por favor, inténtelo de nuevo más tarde.');
     }
+
+    public function enviarProducto(EmailRequest $request)
+    {
+        try {
+            if (!auth()->check()) {
+                return redirect()->route('login')->with('deleted', 'Debe iniciar sesión para enviar el correo electrónico.');
+            }
+
+            $request->validate([
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'message' => 'required|string',
+                'subject' => 'required|string',
+            ]);
+
+            $nombre = $request->first_name;
+            $apellido = $request->last_name;
+            $mensaje = $request->message;
+            $motivo = $request->subject;
+            $send_mail = 'henryrm8@gmail.com';
+
+            Mail::to($send_mail)->send(new EnviarCorreo($nombre, $apellido, $mensaje, $motivo));
+
+            return redirect()->back()->with('message', '¡El correo electrónico ha sido enviado con éxito!');
+        } catch (Exception $e) {
+            $errorMessage = $e->getMessage();
+
+            LogError::create([
+                'message' => 'Error al enviar correo de solicitud',
+                'user_email' => auth()->user()->email,
+                'user_name' => auth()->user()->name,
+                'exception' => $errorMessage,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+        return redirect()->back()->with('deleted', 'Ha ocurrido un error al enviar el correo electrónico. Por favor, inténtelo de nuevo más tarde.');
+    }
 }
